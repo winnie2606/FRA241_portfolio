98487a009ffec4636ef143b78fa77f3f0c3d1157
iampopperzz <popperzaza@gmail.com>
2017-10-31 10:45:21 +0700

    pop

--- app/test.py
+++ app/test.py
@@ -1,5 +1,5 @@
 from getDatabase import *
-studenID = "59340500021"
+studenID = "59340500035"
 term = "1/2559"
 
 s = Get_Academic(studenID,term)
@@ -12,13 +12,13 @@ returnda = return_data(studenID)
 
 add = Add_Method(studenID)
 add.id_stu(Profile)
-add.name("")
-
-print("Profile: ")
-print(returnda.DicPro(re.name(), re.surname(), re.date(), re.birth(), re.nation(), re.education(), re.disease(), re.PhoneEmer(), re.Phonestu(), re.address(), re.email()))
-print("Activity: ")
-print(returnda.DicAct(re.Act_name(), re.Act_des(), re.Act_photo(), re.Act_type(), re.Act_advisor(), re.Act_Date(), re.Act_file(), re.Act_confirm()))
-print("output_term: ")
-print(o.output_term(s.get_id_subject(),t.get_nameSubject(),t.get_credit(),s.get_grade()))
-print("output_sum: ")
-print(p.output_sum(s.get_this_semester_credit(),s.get_GPA(),s.get_cumulative_credit(),s.get_GPAX()))
+# add.name("")
+#
+# print("Profile: ")
+# print(returnda.DicPro(re.name(), re.surname(), re.date(), re.birth(), re.nation(), re.education(), re.disease(), re.PhoneEmer(), re.Phonestu(), re.address(), re.email()))
+# print("Activity: ")
+# print(returnda.DicAct(re.Act_name(), re.Act_des(), re.Act_photo(), re.Act_type(), re.Act_advisor(), re.Act_Date(), re.Act_file(), re.Act_confirm()))
+# print("output_term: ")
+# print(o.output_term(s.get_id_subject(),t.get_nameSubject(),t.get_credit(),s.get_grade()))
+# print("output_sum: ")
+# print(p.output_sum(s.get_this_semester_credit(),s.get_GPA(),s.get_cumulative_credit(),s.get_GPAX()))
